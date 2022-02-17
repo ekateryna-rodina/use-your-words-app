fad33767319ab04ebb4ed549f578dbde1d26cb79
ekateryna-rodina <work.rodina@gmail.com>
2022-02-17 18:05:59 -0500

    TK-0022 setup admin with chakra ui

---
+++ admin/src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@chakra-ui/react";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <Container maxWidth="container.xl">
+      <h1>Hello</h1>
+    </Container>
+  );
+}
+
+export default App;
