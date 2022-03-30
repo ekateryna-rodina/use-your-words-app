44c818cfc7fcbb5b9b53495bd849cd62e319e925
ekateryna-rodina <work.rodina@gmail.com>
2022-03-30 13:41:00 -0400

    create quiz api(call from the service)

--- server/src/services/questions.service.ts
+++ server/src/services/questions.service.ts
@@ -1,11 +1,29 @@
 import ApiError from "../error/apiError";
 import db from "../models";
+import { QuestionType } from "../types/Question";
+import { ExistingWord } from "../types/Word";
+import QuestionsFactory from "./questionsFactory";
 
 const generateQuizQuestions = async (wordIds: string[]) => {
   let quizQuestions = [];
+  const questionTypes = Object.keys(QuestionType).filter(
+    (key) => !isNaN(+QuestionType[key])
+  );
   try {
-    const words = await db.Word.findAll({ where: { id: { $in: wordIds } } });
-    words.forEach((word) => {});
+    const items = await db.Word.findAll({ where: { id: { $in: wordIds } } });
+    for (let item of items) {
+      for (let qt of questionTypes) {
+        var questionType: QuestionType =
+          QuestionType[qt as keyof typeof QuestionType];
+        const newQuestion = QuestionsFactory(
+          questionType,
+          item as ExistingWord,
+          items.filter((w: ExistingWord) => w.word !== item.word)
+        );
+        quizQuestions.push(newQuestion);
+      }
+    }
+    return quizQuestions;
   } catch (error) {
     throw new ApiError(500, error.message);
   }
