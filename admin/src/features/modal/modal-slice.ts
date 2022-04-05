b1d7f8ccb991121747fa82549812ec6838d8c472
ekateryna-rodina <work.rodina@gmail.com>
2022-04-04 22:34:08 -0400

    TK-0001 redux setup

---
+++ admin/src/features/modal/modal-slice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface ModalState {
+  isOpen: boolean;
+}
+
+const initialState: ModalState = {
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    open(state) {
+      state.isOpen = true;
+    },
+    close(state) {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { open, close } = modalSlice.actions;
+export default modalSlice.reducer;
