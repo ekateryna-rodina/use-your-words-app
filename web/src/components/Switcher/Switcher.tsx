0c95a6afa6e8469530b4837dbd6352dc614c8318
ekateryna-rodina <work.rodina@gmail.com>
2022-04-06 23:08:13 -0400

    TK-1002 user settings (dark/light mode, added icons)

--- web/src/components/Switcher/Switcher.tsx
+++ web/src/components/Switcher/Switcher.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { toggle } from "../../features/theme/theme-slice";
+import MoonIcon from "../icons/MoonIcon";
+import SunIcon from "../icons/SunIcon";
 
 const Switcher = () => {
   const dispatch = useAppDispatch();
@@ -9,7 +11,8 @@ const Switcher = () => {
     dispatch(toggle());
   };
   return (
-    <>
+    <div className="flex flex-row items-center">
+      <SunIcon fill={isDark ? "fill-white" : "fill-gray-300"} />
       <input
         type="checkbox"
         id="toggle"
@@ -17,15 +20,16 @@ const Switcher = () => {
         onChange={toggleMode}
       />
       <label htmlFor="toggle">
-        <div className="w-10 h-6 rounded-full flex items-center bg-gray-300 p-1 dark:bg-dark-800">
+        <div className="w-10 h-6 rounded-full flex items-center bg-gray-300 p-1 mx-1 ease-in duration-200 dark:bg-dark-500">
           <div
             className={`w-4 h-4 bg-gray-100 shadow-md rounded-full ease-in duration-200 ${
               isDark ? "translate-x-4" : ""
-            } dark:bg-dark-700`}
+            } dark:bg-dark-800`}
           ></div>
         </div>
       </label>
-    </>
+      <MoonIcon fill={isDark ? "fill-white" : "fill-gray-300"} />
+    </div>
   );
 };
 
