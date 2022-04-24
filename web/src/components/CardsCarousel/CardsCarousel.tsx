47e3ff149540cab03458c5dabc7bd9dd327df78e
ekateryna-rodina <work.rodina@gmail.com>
2022-04-24 14:30:52 -0400

    fixed carousel

--- web/src/components/CardsCarousel/CardsCarousel.tsx
+++ web/src/components/CardsCarousel/CardsCarousel.tsx
@@ -1,9 +1,6 @@
 import React, { MutableRefObject, useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { QuestionType } from "use-your-words-common";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { store } from "../../app/store";
 import { setCurrentChallengeIndex } from "../../features/practice/practice-slice";
 import {
   setAnswered,
@@ -19,9 +16,141 @@ import SimpleMultiselectChallenge from "../Challenges/SimpleMultiselectChallenge
 import TypeWordByMeaning from "../Challenges/TypeWordByMeaning";
 import TypeWordByPronunciation from "../Challenges/TypeWordByPronunciation";
 
-const Challenge: React.FC<PortalProps> = (props) => {
-  return ReactDOM.createPortal(props.children, props.target);
+type ChallengeProps = {
+  currentChallengeIndex: number;
+  parent: MutableRefObject<HTMLDivElement | null>;
 };
+const Challenge = React.memo(
+  ({ currentChallengeIndex, parent }: ChallengeProps) => {
+    const { currentQuizChallenges, currentQuizChallengeIds } = useAppSelector(
+      (state) => state.practice
+    );
+
+    const renderChallenge = (
+      index: number,
+      parent: React.MutableRefObject<HTMLDivElement | null>
+    ): React.ReactNode => {
+      if (index === null || !parent?.current) {
+        return <></>;
+      }
+      const currentChallengeId = currentQuizChallengeIds[index];
+      const currentChallenge = currentQuizChallenges[currentChallengeId];
+      const { __type, question, answer, options, transcription } =
+        currentChallenge;
+
+      switch (__type) {
+        case QuestionType.FillGap:
+          return (
+            <FillGap
+              phrase={question as string}
+              answer={answer as string}
+              challengeId={currentChallengeId}
+            />
+          );
+        case QuestionType.Pronounce:
+          return (
+            <Pronounce
+              fileUrl={question as string}
+              challengeId={currentChallengeId}
+              transcription={transcription as string}
+              word={answer as string}
+            />
+          );
+        case QuestionType.TypeWordByPronunciation:
+          return (
+            <TypeWordByPronunciation
+              fileUrl={question as string}
+              answer={answer as string}
+              challengeId={currentChallengeId}
+              transcription={transcription as string}
+            />
+          );
+        case QuestionType.TypeWordByMeaning:
+          return (
+            <TypeWordByMeaning
+              meaning={question as string}
+              answer={answer as string}
+              challengeId={currentChallengeId}
+            />
+          );
+        case QuestionType.ChooseMeaningByWord:
+          return (
+            <ChooseMeaningByWord
+              question={question as string}
+              answer={answer as string}
+              options={options as string[]}
+              challengeId={currentChallengeId}
+            />
+          );
+        case QuestionType.ChooseWordByMeaning:
+          return (
+            <SimpleMultiselectChallenge
+              title={__type}
+              question={question as string}
+              answer={answer as string}
+              options={options as string[]}
+              challengeId={currentChallengeId}
+            />
+          );
+        case QuestionType.ConnectWordsWithMeanings:
+          return (
+            <ConnectChallenge
+              question={
+                question as {
+                  words: string[];
+                  [k: string]: string[];
+                }
+              }
+              answer={answer as Record<string, string>}
+            />
+          );
+        case QuestionType.ChooseSynonymByWord:
+          return (
+            <SimpleMultiselectChallenge
+              title={__type}
+              question={question as string}
+              answer={answer as string}
+              options={options as string[]}
+              challengeId={currentChallengeId}
+            />
+          );
+        case QuestionType.ChooseAntonymByWord:
+          return (
+            <SimpleMultiselectChallenge
+              title={__type}
+              question={question as string}
+              answer={answer as string}
+              options={options as string[]}
+              challengeId={currentChallengeId}
+            />
+          );
+        case QuestionType.ChooseWordBySynonym:
+          return (
+            <SimpleMultiselectChallenge
+              title={__type}
+              question={question as string}
+              answer={answer as string}
+              options={options as string[]}
+              challengeId={currentChallengeId}
+            />
+          );
+        case QuestionType.ChooseWordByAntonym:
+          return (
+            <SimpleMultiselectChallenge
+              title={__type}
+              question={question as string}
+              answer={answer as string}
+              options={options as string[]}
+              challengeId={currentChallengeId}
+            />
+          );
+        default:
+          return <></>;
+      }
+    };
+    return <>{renderChallenge(currentChallengeIndex, parent)}</>;
+  }
+);
 
 export interface PortalProps {
   target: Element;
@@ -35,175 +164,33 @@ const CardsCarousel = () => {
     currentQuizChallengeIds,
     currentChallengeIndex,
   } = useAppSelector((state) => state.practice);
-
   const dispatch = useAppDispatch();
   let frontRef = useRef<HTMLDivElement | null>(null);
   let middleRef = useRef<HTMLDivElement | null>(null);
   let backRef = useRef<HTMLDivElement | null>(null);
-
+  const [swapBack, setSwapBack] =
+    useState<React.MutableRefObject<HTMLDivElement> | null>(null);
   const [refsArray, setRefsArray] = useState<
     React.MutableRefObject<HTMLDivElement | null>[]
   >([frontRef, middleRef, backRef]);
 
-  const [challengeIdx, setChallengeIdx] = useState<number[]>([0, 1, 2]);
-
-  const renderChallenge = (
-    index: number,
-    parent: React.MutableRefObject<HTMLDivElement | null>
-  ): React.ReactNode => {
-    if (index === null || !parent?.current) {
-      return <></>;
-    }
-    const currentChallengeId = currentQuizChallengeIds[index];
-    const currentChallenge = currentQuizChallenges[currentChallengeId];
-    const { __type, question, answer, options, transcription } =
-      currentChallenge;
-
-    switch (__type) {
-      case QuestionType.FillGap:
-        return (
-          <FillGap
-            phrase={question as string}
-            answer={answer as string}
-            challengeId={currentChallengeId}
-          />
-        );
-      case QuestionType.Pronounce:
-        return (
-          <Pronounce
-            fileUrl={question as string}
-            challengeId={currentChallengeId}
-            transcription={transcription as string}
-            word={answer as string}
-          />
-        );
-      case QuestionType.TypeWordByPronunciation:
-        return (
-          <TypeWordByPronunciation
-            fileUrl={question as string}
-            answer={answer as string}
-            challengeId={currentChallengeId}
-            transcription={transcription as string}
-          />
-        );
-      case QuestionType.TypeWordByMeaning:
-        return (
-          <TypeWordByMeaning
-            meaning={question as string}
-            answer={answer as string}
-            challengeId={currentChallengeId}
-          />
-        );
-      case QuestionType.ChooseMeaningByWord:
-        return (
-          <ChooseMeaningByWord
-            question={question as string}
-            answer={answer as string}
-            options={options as string[]}
-            challengeId={currentChallengeId}
-          />
-        );
-      case QuestionType.ChooseWordByMeaning:
-        return (
-          <SimpleMultiselectChallenge
-            title={__type}
-            question={question as string}
-            answer={answer as string}
-            options={options as string[]}
-            challengeId={currentChallengeId}
-          />
-        );
-      case QuestionType.ConnectWordsWithMeanings:
-        return (
-          <ConnectChallenge
-            question={
-              question as {
-                words: string[];
-                [k: string]: string[];
-              }
-            }
-            answer={answer as Record<string, string>}
-          />
-        );
-      case QuestionType.ChooseSynonymByWord:
-        return (
-          <SimpleMultiselectChallenge
-            title={__type}
-            question={question as string}
-            answer={answer as string}
-            options={options as string[]}
-            challengeId={currentChallengeId}
-          />
-        );
-      case QuestionType.ChooseAntonymByWord:
-        return (
-          <SimpleMultiselectChallenge
-            title={__type}
-            question={question as string}
-            answer={answer as string}
-            options={options as string[]}
-            challengeId={currentChallengeId}
-          />
-        );
-      case QuestionType.ChooseWordBySynonym:
-        return (
-          <SimpleMultiselectChallenge
-            title={__type}
-            question={question as string}
-            answer={answer as string}
-            options={options as string[]}
-            challengeId={currentChallengeId}
-          />
-        );
-      case QuestionType.ChooseWordByAntonym:
-        return (
-          <SimpleMultiselectChallenge
-            title={__type}
-            question={question as string}
-            answer={answer as string}
-            options={options as string[]}
-            challengeId={currentChallengeId}
-          />
-        );
-      default:
-        return <></>;
-    }
-  };
-
+  const [refIndexData, setRefIndexData] = useState<WeakMap<
+    React.MutableRefObject<HTMLDivElement>,
+    number
+  > | null>(null);
+  let timer: ReturnType<typeof setTimeout>;
   const updateStyles = () => {
     const frontTo = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
+      const nextChallengeIndex = (currentChallengeIndex ?? 0) + 2;
       ref.current?.classList.add("next");
-      const nextChallengeIndex = (currentChallengeIndex ?? 0) + 3;
-      if (nextChallengeIndex < currentQuizChallengeIds.length) {
-        setTimeout(() => {
+
+      if (nextChallengeIndex < currentQuizChallengeIds.length - 1) {
+        timer = setTimeout(() => {
+          setSwapBack(ref as React.MutableRefObject<HTMLDivElement>);
           ref.current?.classList.add("back", "hidden");
           ref.current?.classList.remove("front", "next");
           ref.current?.classList.remove("hidden");
-          console.log(
-            ref,
-            ref.current?.children,
-            ref.current?.children.length,
-            ref.current?.children[0]
-          );
-          // (ref.current as Element).innerHTML = "";
-          ReactDOM.unmountComponentAtNode(ref.current?.children[0] as Element);
-          console.log("removved");
-          const newChallengeNode = renderChallenge(
-            nextChallengeIndex,
-            ref as React.MutableRefObject<HTMLDivElement>
-          );
-
-          ReactDOM.render(
-            <Provider store={store}>
-              {/* <Challenge
-                target={ref.current as Element}
-                children={newChallengeNode}
-              /> */}
-              <div>any</div>
-            </Provider>,
-            ref.current
-          );
-        }, 1000);
+        }, 700);
       }
     };
     const middleTo = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
@@ -221,6 +208,28 @@ const CardsCarousel = () => {
   };
 
   useEffect(() => {
+    if (!swapBack) return;
+    const nextChallengeIndex = (currentChallengeIndex ?? 0) + 2;
+    if (nextChallengeIndex >= currentQuizChallengeIds.length) return;
+    setRefIndexData((oldState) => {
+      oldState?.set(
+        swapBack as React.MutableRefObject<HTMLDivElement>,
+        nextChallengeIndex
+      );
+      return oldState;
+    });
+    setSwapBack(null);
+    // eslint-disable-next-line
+  }, [swapBack]);
+  useEffect(() => {
+    if (!frontRef || !middleRef || !backRef) return;
+    const initRefIndexData = new WeakMap();
+    initRefIndexData.set(frontRef, 0);
+    initRefIndexData.set(frontRef, 1);
+    initRefIndexData.set(frontRef, 2);
+    setRefIndexData(initRefIndexData);
+  }, [frontRef, middleRef, backRef]);
+  useEffect(() => {
     if (!isNext) return;
     updateStyles();
     dispatch(setCurrentChallengeIndex((currentChallengeIndex ?? 0) + 1));
@@ -235,34 +244,50 @@ const CardsCarousel = () => {
     // eslint-disable-next-line
   }, [isNext]);
   useEffect(() => {
-    setChallengeIdx((oldIdx) => {
-      const newIdx = [...oldIdx];
-      return newIdx;
-    });
-
-    //eslint-disable-next-line
-  }, [currentChallengeIndex]);
-
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, []);
+  const getIndexByRef = (ref: React.MutableRefObject<HTMLDivElement>) => {
+    return refIndexData?.get(ref) ?? 0;
+  };
   return (
     <div className="front-card-container">
       <div className="w-full h-full relative">
-        <Card ref={frontRef} position={PracticeCardPosition.Front}>
-          {renderChallenge(
-            0,
-            frontRef as MutableRefObject<HTMLDivElement | null>
-          )}
+        <Card
+          ref={frontRef as React.MutableRefObject<HTMLDivElement>}
+          position={PracticeCardPosition.Front}
+        >
+          <Challenge
+            currentChallengeIndex={getIndexByRef(
+              frontRef as MutableRefObject<HTMLDivElement>
+            )}
+            parent={frontRef as MutableRefObject<HTMLDivElement>}
+          />
         </Card>
-        <Card ref={middleRef} position={PracticeCardPosition.Middle}>
-          {renderChallenge(
-            1,
-            middleRef as MutableRefObject<HTMLDivElement | null>
-          )}
+        <Card
+          ref={middleRef as React.MutableRefObject<HTMLDivElement>}
+          position={PracticeCardPosition.Middle}
+        >
+          <Challenge
+            currentChallengeIndex={getIndexByRef(
+              middleRef as MutableRefObject<HTMLDivElement>
+            )}
+            parent={middleRef as MutableRefObject<HTMLDivElement | null>}
+          />
         </Card>
-        <Card ref={backRef} position={PracticeCardPosition.Back}>
-          {renderChallenge(
-            2,
-            backRef as MutableRefObject<HTMLDivElement | null>
-          )}
+        <Card
+          ref={backRef as React.MutableRefObject<HTMLDivElement>}
+          position={PracticeCardPosition.Back}
+        >
+          <Challenge
+            currentChallengeIndex={getIndexByRef(
+              backRef as MutableRefObject<HTMLDivElement>
+            )}
+            parent={backRef as MutableRefObject<HTMLDivElement | null>}
+          />
         </Card>
       </div>
     </div>
